663d0a83c0aafc274fd9779f76386a2b8482ad4a
Vincent <vincentbusiness55@gmail.com>
2021-11-13 20:11:07 -0800

    Bump!

--- pycord/audio/__init__.py
+++ pycord/audio/__init__.py
@@ -1,8 +1,8 @@
 __title__ = 'audio'
 __author__ = 'pycord'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2019-2021 (c) PythonistaGuild'
-__version__ = '0.9.10'
+__copyright__ = 'Copyright 2021 (c) Pycord'
+__version__ = '1.0.0'
 
 from .client import Client
 from .errors import *
