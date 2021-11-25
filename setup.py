8d83fb49e53e51861ed945a4d9ca414ea036ae46
RPS <vincentbusiness55@gmail.com>
2021-11-24 23:05:00 -0800

    Ehm
    So pycordutils is already on pypi

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ with open(ROOT / "pycord" / "audio" / "__init__.py", encoding="utf-8") as f:
 
 
 setuptools.setup(
-    name="PycordUtils",
+    name="Pycord-Utils",
     author="pycord",
     url="https://github.com/pycord/utilitys",
     version=VERSION,
