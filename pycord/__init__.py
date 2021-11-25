2a892d4f1f89673d6d1f7f164b5208c9d6d87d0a
RPS <vincentbusiness55@gmail.com>
2021-11-24 22:55:58 -0800

    Finalizing

---
+++ pycord/__init__.py
@@ -0,0 +1,11 @@
+"""
+pycord
+~~~~~~~
+Utilitys For The Pycord Discord Library 
+"""
+
+__title__ = "PycordUtils"
+__author__ = "pycord"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021 (c) Pycord"
+__version__ = "1.1.0"
