4b2b0d339588a3a97d3a7c76dca9b6a9ae1b5317
AlexyDaCoder <81732383+alexyy802@users.noreply.github.com>
2021-11-29 18:42:45 +0700

    pycord.ext.alternatives (#5)
    * Add files via upload
    
    * Update pycord/ext/alternatives/DEV/menus_remove_reaction.py
    
    * Update pycord/ext/alternatives/DEV/menus_remove_reaction.py
    
    * Update pycord/ext/alternatives/__init__.py
    
    * Update pycord/ext/alternatives/__init__.py
    
    * Update pycord/ext/alternatives/__init__.py
    
    * Update pycord/ext/alternatives/role.py
    
    * Update pycord/ext/alternatives/role.py
    
    * Update pycord/ext/alternatives/silent_delete.py
    
    * Update pycord/ext/alternatives/specific_error_handler.py
    
    * Update pycord/ext/alternatives/webhook_channel.py
    
    * Update pycord/ext/alternatives/__init__.py
    
    * Update pycord/ext/alternatives/asset_converter.py
    
    * Update pycord/ext/alternatives/binary_checks.py
    
    * Update pycord/ext/alternatives/binary_checks.py
    
    * Update pycord/ext/alternatives/category_channel.py
    
    * Update pycord/ext/alternatives/class_command.py
    
    * Update pycord/ext/alternatives/command_piping.py
    
    * Update pycord/ext/alternatives/literal_converter.py
    
    * Update pycord/ext/alternatives/command_suffix.py
    
    * Update pycord/ext/alternatives/command_suffix.py
    
    * Update pycord/ext/alternatives/jump_url.py
    
    * Update pycord/ext/alternatives/converter_dict.py
    
    * Update pycord/ext/alternatives/converter_dict.py
    
    * Update pycord/ext/alternatives/fix_4098.py
    
    * Update pycord/ext/alternatives/fix_4098.py
    
    * Update pycord/ext/alternatives/guild_converter.py
    
    * Update pycord/ext/alternatives/guild_converter.py
    
    * Update pycord/ext/alternatives/inline_bot_commands.py
    
    * Update pycord/ext/alternatives/int_map.py
    
    * Update pycord/ext/alternatives/literal_converter.py
    
    * Update pycord/ext/alternatives/object_contains.py
    
    * Update pycord/ext/alternatives/material_colours.py
    
    * Update pycord/ext/alternatives/message_eq.py
    
    * Update pycord/ext/alternatives/object_contains.py
    
    * Update pycord/ext/alternatives/messageable_wait_for.py
    
    * Update pycord/ext/alternatives/messageable_wait_for.py
    
    * Update pycord/ext/alternatives/object_contains.py
    
    * Update pycord/ext/alternatives/messageable_wait_for.py
    
    * Update pycord/ext/alternatives/messageable_wait_for.py
    
    * Update pycord/ext/alternatives/object_contains.py
    
    * Update pycord/ext/alternatives/object_contains.py
    
    * Update pycord/ext/alternatives/object_contains.py
    
    * Update pycord/ext/alternatives/object_contains.py
    
    * Update pycord/ext/alternatives/object_contains.py
    
    * Apply suggestions from code review
    
    Co-authored-by: RPS <vincentbusiness55@gmail.com>

---
+++ pycord/ext/alternatives/__init__.py
@@ -0,0 +1,11 @@
+"""
+pycord.ext.alternatives
+~~~~~~~~~~~~~~~~
+Pycord extension with additional and alternative features.
+
+:copyright: 2021 Pycord & Ext-Creators
+:license: Apache-2.0
+"""
+
+
+version = "2021.4.13"
