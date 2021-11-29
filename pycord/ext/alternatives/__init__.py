d6a39e7b4b04feafe6a369ae7a483946789f5e37
RPS <vincentbusiness55@gmail.com>
2021-11-29 19:11:04 +0800

    Update pycord/ext/alternatives/__init__.py

--- pycord/ext/alternatives/__init__.py
+++ pycord/ext/alternatives/__init__.py
@@ -1,4 +1,11 @@
-import collections
+"""
+pycord.ext.alternatives
+~~~~~~~~~~~~~~~~
+Pycord extension with additional and alternative features.
+
+:copyright: 2021 Pycord & Ext-Creators
+:license: Apache-2.0
+"""
 
 
 version = "2021.4.13"
