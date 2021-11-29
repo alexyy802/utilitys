02c244bcdef4b00b95316c2ec208b5748268d10a
Vincent <vincentbusiness55@gmail.com>
2021-11-29 16:07:55 +0800

    Fix

--- docs/_static/scorer.js
+++ docs/_static/scorer.js
@@ -36,7 +36,7 @@ Scorer = {
         // only inflate the score of things that are actual API reference things
         const [, title, , , score] = result;
 
-        if (pattern !== null && title.startsWith('nextcord.')) {
+        if (pattern !== null && title.startsWith('pycord.')) {
             let _score = __score(title, pattern);
             if (_score === Number.MAX_VALUE) {
                 return score;
